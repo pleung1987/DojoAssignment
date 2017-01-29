65127c346aad2c7c31d306e591b21e29524645ce
Patrick Leung <pleung1987@gmail.com>
2017-01-29 13:18:04 +0800

    Added IDoDeclare.js assignment

---
+++ Webfundementals/Javascript/IDoDeclare.js
@@ -0,0 +1,23 @@
+var num1= 1230;
+var num2=-9870;
+var num3= 0.314;
+var num4= -0.098;
+
+console.log(num1,num2,num3,num4);
+
+var string1= "Jason Borne";
+var string2="Can I be your boyfriend?";
+var string3="I'm sorry, its just me not you..."
+
+console.log("who the hell are you?", "I am...", string1);
+console.log(string2, "ofcorse!");
+console.log("Why are you leaving me?",string3);
+
+var CodersAreNerds= false;
+var CodersAreCool= true;
+
+console.log(CodersAreNerds);
+console.log(CodersAreCool);
+
+var x;
+console.log(x);
