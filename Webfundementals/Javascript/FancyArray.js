9996a2564a9fca0132eec8617f2afa51c5e33584
Patrick Leung <pleung1987@gmail.com>
2017-01-31 07:24:55 +0800

    added Fancy Array assignment

---
+++ Webfundementals/Javascript/FancyArray.js
@@ -0,0 +1,9 @@
+function PrintFancyArray(){
+var arr=[];
+arr.push("0 -> James");
+arr.push("1-> Jill");
+arr.push("2-> Jane");
+arr.push("3-> Jack");
+console.log(arr);
+}
+PrintFancyArray();
---
+++ Webfundementals/Javascript/RangePrint.js
@@ -0,0 +1,7 @@
+// PrintRange(2,10,2)
+function PrintRange(){
+for(var i=2;i<10;i+=2){
+  console.log(i)
+}
+}
+PrintRange();
